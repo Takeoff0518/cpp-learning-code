c988c90181f457ec3ad621f8a6791f0cd131043c
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2023-10-17 21:17:50 +0800

    Update P7913 P8817

--- P7913.cpp
+++ P7913.cpp
@@ -2,7 +2,8 @@
 #include<iostream>
 #include<cstring>
 #include<cstdio>
-#define N (1005)
+#include<queue>
+#define N (100005)
 #define M (1005)
 #define ll long long
 #define INF (0x3f3f3f3f)
@@ -11,7 +12,42 @@
 #define ls(xxx) (xxx)<<1
 #define rs(xxx) (xxx)<<1|1
 using namespace std;
+int n,m1,m2;
+struct node{
+	int x,y;
+	bool operator < (const node&xxxxx) const{
+		return xxxxx.x>x;
+	}
+}a[N],b[N]; 
+int res1[N],res2[N];
+void solve(node *t,int m,int *res){
+	priority_queue<PII,vector<PII>,greater<PII> > lq;
+	priority_queue<int,vector<int>,greater<int> > wq;
+	for(int i=1;i<=n;i++) wq.push(i);
+	for(int i=1;i<=m;i++){
+		while(!lq.empty() && t[i].x>=lq.top().first){
+			wq.push(lq.top().second);
+			lq.pop();
+		}
+		if(wq.empty()) continue;
+		int dest=wq.top(); wq.pop();
+		res[dest]++;
+		lq.push({t[i].y,dest});
+	}
+	for(int i=1;i<=n;i++) res[i]+=res[i-1];
+}
 int main(){
-	
+	scanf("%d %d %d",&n,&m1,&m2);
+	for(int i=1;i<=m1;i++)
+		scanf("%d %d",&a[i].x,&a[i].y);
+	for(int i=1;i<=m2;i++)
+		scanf("%d %d",&b[i].x,&b[i].y);
+	sort(a+1,a+m1+1); sort(b+1,b+m2+1);
+	solve(a,m1,res1); solve(b,m2,res2);
+	int ans=0;
+	for(int i=0;i<=n;i++){
+		ans=max(ans,res1[i]+res2[n-i]);
+	}
+	printf("%d\n",ans);
 	return 0;
 }
--- P8817.cpp
+++ P8817.cpp
@@ -4,17 +4,17 @@
 #include<cstdio>
 #include<queue>
 #include<set>
-#define N (1005)
-#define M (1005)
+#define N (2505)
+#define M (20005)
 #define ll long long
 #define INF (0x3f3f3f3f)
 #define mod (998244353)
-#define PII pair<int,int>
+#define PII pair<ll,int>
 #define ls(xxx) (xxx)<<1
 #define rs(xxx) (xxx)<<1|1
 using namespace std;
 int n,m,k;
-int s[N];
+ll s[N];
 int dis[N][N];
 struct E{
     int nxt,to;
@@ -26,13 +26,11 @@ void add(int from,int to){
     edge[num_edge].to=to;
     head[from]=num_edge;
 }
-int rank[N][5];
 bool vis[N];
-void bfs(int *d,int s){
+void bfs(int s){
     queue<int> q;
-    memset(d,0x3f,sizeof(d));
     memset(vis,0,sizeof(vis));
-    d[s]=0;
+    dis[s][s]=0;
     q.push(s);
     while(!q.empty()){
         int t=q.front(); q.pop();
@@ -40,18 +38,48 @@ void bfs(int *d,int s){
         vis[t]=1;
         for(int i=head[t];i;i=edge[i].nxt){
             int j=edge[i].to;
-            if(d[j]>d[t]+1){
-                d[j]=d[t]+1;
+            if(dis[s][j]>dis[s][t]+1){
+                dis[s][j]=dis[s][t]+1;
                 if(!vis[j]) q.push(j);
             }
         }
     }
 }
+set<PII> bs[N];
+ll ans=0;
 int main(){
     scanf("%d %d %d",&n,&m,&k);
-    for(int i=1;i<n;i++) scanf("%d",&s[i]);
+    for(int i=2;i<=n;i++) scanf("%lld",&s[i]);
     for(int i=1,u,v;i<=m;i++){
         scanf("%d %d",&u,&v);
+        add(u,v); add(v,u); 
     }
+    memset(dis,0x3f,sizeof(dis));
+    for(int i=1;i<=n;i++)	bfs(i);
+	for(int i=2;i<=n;i++){
+		for(int j=2;j<=n;j++){
+			if(i==j) continue;
+			if(dis[i][j]<=k+1 && dis[1][j]<=k+1){
+				bs[i].insert({s[j],j});
+			}
+			if(bs[i].size()>3){
+				bs[i].erase(bs[i].begin());
+			}
+		}
+	}
+	for(int b=2;b<=n;b++){
+		for(int c=2;c<=n;c++){
+			if(dis[b][c]>k+1 || b==c) continue;
+			for(PII a:bs[b]){
+				if(a.second==b || a.second==c) continue;
+				for(PII d:bs[c]){
+					if(d.second==b || d.second==c || d.second==a.second)
+						continue;
+					ans=max(ans,s[b]+s[c]+s[a.second]+s[d.second]);
+				}
+			}
+		}
+	}
+	printf("%lld\n",ans);
     return 0;
 }
